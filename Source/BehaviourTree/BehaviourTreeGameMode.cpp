d67e9911966b4ad41118ec335e81ed7fcfdd6458
Arjun Chauhan <ArChauhan@Sumo-Digital.com>
2019-11-25 15:03:04 +0530

    Started AI Tutorial

---
+++ Source/BehaviourTree/BehaviourTreeGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "BehaviourTreeGameMode.h"
+#include "BehaviourTreeCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ABehaviourTreeGameMode::ABehaviourTreeGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
